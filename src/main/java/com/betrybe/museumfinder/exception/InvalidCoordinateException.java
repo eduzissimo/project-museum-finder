0c299316ee7ff5e5876c76023aef04eb9cac1045
eduzissimo <eduardo.alves14@hotmail.com>
2024-08-09 17:05:06 -0300

    Feat: Req. 3

---
+++ src/main/java/com/betrybe/museumfinder/exception/InvalidCoordinateException.java
@@ -0,0 +1,10 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * InvalidCoordinateException class.
+ */
+public class InvalidCoordinateException extends RuntimeException {
+  public InvalidCoordinateException(String message) {
+    super(message);
+  }
+}
